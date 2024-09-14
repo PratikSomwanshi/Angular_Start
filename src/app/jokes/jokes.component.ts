eb76b57db87cb92d7a4d8708e5988c84ee8fb53c
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-09-14 17:55:55 +0530

    fix: add loading and default msg

--- src/app/jokes/jokes.component.ts
+++ src/app/jokes/jokes.component.ts
@@ -8,8 +8,21 @@ import { JokesService } from '../jokes.service';
 })
 export class JokesComponent {
   ola: any[] = [];
+  loading = false;
 
-  constructor(private js: JokesService) {
-    this.js.getData().subscribe((data: any) => (this.ola = data));
+  constructor(private js: JokesService) {}
+
+  getData() {
+    this.loading = true;
+    this.js.getData().subscribe(
+      (data: any) => {
+        this.ola = data;
+        this.loading = false;
+      },
+      (err) => {
+        console.log(err);
+        this.loading = false;
+      }
+    );
   }
 }
